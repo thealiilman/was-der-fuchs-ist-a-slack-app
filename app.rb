1747cf605ab71fcd5c8dc293faa149a798be3030
thealiilman <aliilman532@gmail.com>
2019-11-29 10:48:34 +0800

    Added a post request to root.

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require 'sinatra'
+require 'sinatra/json'
 require "sinatra/reloader" if development?
 
 get '/' do
   'MAYBE I JUST WANNA FLY'
+end
+
+post '/' do
+  json({
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "_D'you know what I mean, yeah yeah_"
+        }
+      },
+    ]
+  })
 end
